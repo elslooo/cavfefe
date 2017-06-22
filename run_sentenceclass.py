ee061e0d440af8eaf70fc1a50fd2d8669e349483
Philip Botros <philipbotros@live.nl>
2017-06-22 19:42:45 +0200

    Added sentence classifier

--- run_sentenceclass.py
+++ run_sentenceclass.py
@@ -2,12 +2,15 @@ import tensorflow as tf
 import numpy as np
 from idx2word import idx2sentence, embedding_size
 import lib.lm as lm
+from lib.lm.sentenceclass import SentenceClassifier
+from lib.lm.class_reader import SentenceClassReader
 import lib.etc as etc
 
 max_length = 15
+nr_classes = 200
 
-reader = lm.SentenceReader("data/produced_sentences.csv",
-                           embedding_size = embedding_size)
+reader = SentenceClassReader("data/produced_sentences_classifier.csv",
+                        nr_classes, embedding_size = embedding_size)
 
 def get_batch(batch_size):
     return reader.read(lines = batch_size)
@@ -18,7 +21,7 @@ epochs     = int(942878 / batch_size)
 # Network Parameters
 num_hidden = 512 # hidden layer num of features
 
-model = lm.LanguageModel(max_length, embedding_size, num_hidden)
+model = SentenceClassifier(max_length, embedding_size, num_hidden, nr_classes)
 
 init = tf.global_variables_initializer()
 
@@ -27,13 +30,13 @@ with tf.Session() as sess:
 
     for step, pi in etc.range(epochs):
         # Get a batch of training instances.
-        batch_x, batch_y, batch_len, batch_f = get_batch(batch_size)
+        batch_x, batch_y, batch_len = get_batch(batch_size)
 
         # Run optimization op (backprop)
-        model.train(batch_x, batch_f, batch_y, batch_len, sess)
+        model.train(batch_x, batch_y, batch_len, sess)
 
         # Calculate batch accuracy and loss
-        acc, loss = model.evaluate(batch_x, batch_f, batch_y, batch_len, sess)
+        acc, loss = model.evaluate(batch_x, batch_y, batch_len, sess)
 
         print("Iter " + str(1 + step) + " / " + str(epochs) + \
               ", Minibatch Loss= " + \
@@ -42,11 +45,11 @@ with tf.Session() as sess:
               etc.format_seconds(pi.time_remaining()))
 
         # Generate a sample sentence after each 10 iterations.
-        if (1 + step) % 10 == 0:
-            features = [ 0 ] * 5 + [ 1 ] + [ 0 ] * 94
+        # if (1 + step) % 10 == 0:
+        #     features = [ 0 ] * 5 + [ 1 ] + [ 0 ] * 94
 
-            sentence = model.generate(features, sess)
+            # sentence = model.generate(sess)
 
-            print(idx2sentence([ np.argmax(word) for word in sentence ]))
+            # print(idx2sentence([ np.argmax(word) for word in sentence ]))
 
     print("Optimization Finished!")
