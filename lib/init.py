bd2b7f618665a1085e7a598682de33e2086c7030
henglin <henglin.h@gmail.com>
2017-06-27 20:43:05 +0200

    included the cocos crap and fixed the path for the implementation from eccv2016

---
+++ lib/init.py
@@ -0,0 +1,28 @@
+import json
+import sys
+
+# coco_eval_path = '../../data/coco/'
+coco_eval_path = './lib/coco/coco-caption-eval'
+
+# caffe_dir = '../../'
+# sys.path.insert(0, caffe_dir + 'python_layers/')
+# pycaffe_path = '../../python/'
+# cub_features = 'data/CUB_feature_dict.p'
+# bird_anno_path_fg = 'data/descriptions_bird.%s.fg.json'
+
+cache_home = 'generated_sentences/'
+
+def read_json(t_file):
+  j_file = open(t_file).read()
+  return json.loads(j_file)
+
+def save_json(json_dict, save_name):
+  with open(save_name, 'w') as outfile:
+    json.dump(json_dict, outfile)  
+
+def open_txt(t_file):
+  txt_file = open(t_file).readlines()
+  return [t.strip() for t in txt_file]
+
+
+
